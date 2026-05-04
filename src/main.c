740ac1d2f5bb6727aba00d0c06fbc5ac850f3a6c
tagbo-dot <taguiwaboris@gmail.com>
2026-05-04 01:55:53 +0100

    Ajout d'element

--- src/main.c
+++ src/main.c
@@ -3,3 +3,12 @@
 #include "procfs.h"
 #include "options.h"
 #include "format.h"
+int main(int argc, char *argv[])
+{
+    Options opts;
+    int     ret = EXIT_SUCCESS;
+
+    /* ---- Parsing des options et des PIDs ---- */
+    if (parse_options(argc, argv, &opts) != 0)
+        return EXIT_FAILURE;
+}
