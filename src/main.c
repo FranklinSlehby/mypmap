905bb7b590114db13ac492f68ffcb2882be678c5
tagbo-dot <taguiwaboris@gmail.com>
2026-05-12 18:49:06 +0100

    ajout d'element

--- src/main.c
+++ src/main.c
@@ -3,12 +3,35 @@
 #include "procfs.h"
 #include "options.h"
 #include "format.h"
-int main(int argc, char *argv[])
-{
+int main (int argc, char *argv[])
+    {  
     Options opts;
-    int     ret = EXIT_SUCCESS;
-
-    /* ---- Parsing des options et des PIDs ---- */
+    int ret = EXIT_SUCCESS;
     if (parse_options(argc, argv, &opts) != 0)
-        return EXIT_FAILURE;
-}
+       return EXIT_FAILURE;
+    for (int i = 0; i < opts.pid_count; i++)
+        {
+            pid_t    pid = (pid_t)opts.pids[1];
+            ProcInfo info;
+            MapArray maps;
+
+            if (read_proc_info(pid, &info) !=0 || read_maps(pid, &maps) !=0)
+            {
+                fprint(stderr, "mypmap: impossible de lire le PID %d\n", (int)pid);
+                ret = EXIT_FAILURE;
+                continue;
+            }
+            
+            if (opts.show_extended || opts.show_very_extended)
+                read_smaps(pid, &maps);
+
+            print_output(pid, &maps, &info, &opts);
+            free_maps(&maps);
+
+            if (i < opts.pid_count - 1)
+                putchar('\n');
+        }
+        
+        free_options($opts);
+        return ret;
+    }
